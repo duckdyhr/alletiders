5ffb12c2bcd7956f2fed9cc8739b177b84e735b9
Anne Dyhr <Arthur@Arthur.local>
2017-03-24 15:59:19 +0100

    Intet specielt

--- alletiders/App/Javascript/app.js
+++ alletiders/App/Javascript/app.js
@@ -1,1 +1,6 @@
-var app = angular.module('testapp', []);
+//var app = angular.module('testapp', []);
+
+function x(){
+  console.log("Testing script imports");    
+};
+console.log("Testing script imports");
