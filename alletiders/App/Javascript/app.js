cb07f30d5eeda7583553acbe046532e0f71faff5
Anne Dyhr <Arthur@Arthur.local>
2017-03-25 21:31:30 +0100

    Testing af Angular features...

--- alletiders/App/Javascript/app.js
+++ alletiders/App/Javascript/app.js
@@ -1,10 +1,29 @@
-var app = angular.module('app', []);
+(function () {
+    //Wrapper javascript i en closure - apparently a good habit.
+    var app = angular.module('alletiders', []);
 
-function test(){
-  //$('#test').text("Testing app.js");
+    app.controller('testController', function ($scope) {
+        $scope.userid = "Id1";
+        $scope.userpw = "pw";
+        $scope.data = persons;
+    });
+    
+    function person(fname, lname){
+        return {firstname: fname, lastname: lname};  
+    };
+    
+    var persons = [
+        person('Anne', 'Dyhr'), 
+        person('Johan', 'Keller')
+    ];
+})();
+
+//Test functions for Jasmine...
+function test() {
+    //$('#test').text("Testing app.js");
     document.getElementById("test").innerHTML = x;
 };
 
-function add(x, y){
-    return x+y;
+function add(x, y) {
+    return x + y;
 }
