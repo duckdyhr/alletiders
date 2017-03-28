5834afa4c4570393d491634116e4691e34977cb9
Anne Dyhr <Arthur@Arthur.local>
2017-03-28 21:57:07 +0200

    Testing of PHP and adding data to DB done...

--- testPHP/w3Testing.php
+++ testPHP/w3Testing.php
@@ -2,15 +2,22 @@
     $servername = "chat.students.dk";
     $username = "joan";
     $password = "joan9999";
+    $dbname = "joan";
     
     echo "My first echo!\n";
 try{
-    $conn = new PDO("mysql:host=$servername;dbname=joan", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
     echo "Connected successfully\n";
     
-    $sql = "INSERT INTO test(id) VALUES(3)";
-    $conn->exec($sql);
+    //Prepared statement
+    $sql = "SELECT memberId FROM Login";
+    
+    $result = $conn->query($sql);
+    
+    echo json_encode($result) . "\n";
+    
     echo "sql executed successfully\n";
 }catch(PDOException $e){
     echo $sql . "\n" . $e->getMessage() . "\n";
