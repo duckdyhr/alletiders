1be2128baa726d08dd85676d835e4cad93bb1919
Anne Dyhr <Arthur@Arthur.local>
2017-04-06 23:53:44 +0200

    Auto-udfyldte felter done.

--- alletiders/App/Javascript/controller/registrationController.js
+++ alletiders/App/Javascript/controller/registrationController.js
@@ -3,36 +3,35 @@
     var app = angular.module('registration', []);
 
     app.controller('registrationController', function ($scope, $http, userData) {
+        console.log(userData.get());
         $scope.msgSuccess = true;
-
+        
         $http.get("app/ajax/laug.php")
             .then(function (response) {
                 $scope.laug = response.data;
             });
-
-        $scope.frivillige = ["dummy"];
-
-        console.log(userData.get());
-
+        
+        $http.get("app/ajax/membersLaug.php")
+            .then(function(response){
+           $scope.frivillige = response.data; 
+        });
+        
+        $scope.hours = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12];
+        
         var defaultForm = {
             selectedProjectLaug: null,
             selectedFrivillig: null,
-            hours: NaN,
-            date: undefined,
-            author: null
-        }
-
-        $scope.downloadRegistrations = function () {
-            console.log("Funktionen bliver kaldt!")
-            $http.get("app/ajax/csv-handling.php").then(function (response) {
-                console.log(response);
-            });
+            selectedHours: $scope.hours[3],
+            date: new Date(),
+            author: userData.get().id
         }
 
         $scope.form = angular.copy(defaultForm);
 
         $scope.laugChanged = function () {
             console.log("laug changed!");
+            console.log($scope.frivillige);
+            console.log($scope.laug);
         }
 
         $scope.registerTime = function () {
@@ -54,5 +53,12 @@
             $scope.form = angular.copy(defaultForm);
             console.log("empty");
         };
+        
+        $scope.downloadRegistrations = function () {
+            console.log("Funktionen bliver kaldt!")
+            $http.get("app/ajax/csv-handling.php").then(function (response) {
+                console.log(response);
+            });
+        }
     });
 })();
